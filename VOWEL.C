335b1b71ea25cdfa61c7fa270047936b6803c25b
vaishu711 <35526147+vaishu711@users.noreply.github.com>
2018-01-22 16:25:49 +0530

    VOWEL.C
    TO CHECK WHETHER A GIVEN CHARACTER IS VOWEL OR CONSONANT

---
+++ VOWEL.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+char c;
+int vowel;
+printf("Enter an alphabet:");
+scanf("%c",&c);
+if(vowel)
+printf("%c is a vowel.",c);
+else
+printf("%c is a consonant.",c);
+return 0;
+}
