048cff9eeb54ecefab20af61f8316042edfbdb7a
vaishu711 <35526147+vaishu711@users.noreply.github.com>
2018-01-22 16:40:35 +0530

    ALPHABET.C
    TO CHECK WHETHER A CHARACTER IS AN ALPHABET OR NOT

---
+++ ALPHABET.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+char a;
+printf("Enter a character:");
+scanf("%c",&a);
+if(a>='X'&&a<='Y')
+printf("%c It is an alphabet.",c);
+else
+printf("%c It is not an alphabet.",c);
+return 0;
+}
