88be06a6e9b0b4324ccafbfcb4a8a1a443835af1
vaishu711 <35526147+vaishu711@users.noreply.github.com>
2018-02-02 16:25:15 +0530

    POWER.C
    TO CALCULATE THE POWER OF A NUMBER

---
+++ POWER.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int base,exponentialvalue ;
+long long result=1;
+printf("Enter a base number:");
+scanf("%d",&base);
+printf("Enter an exponentialvalue:");
+scanf("%d",&exponentialvalue!=0)
+{
+result*=base;
+--exponentialvalue;
+}
+printf("Answer=%lld",result);
+return 0;
+}
