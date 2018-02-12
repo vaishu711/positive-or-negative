7aa5abfbe40c2f7b263d00488914bc0135564279
vaishu711 <35526147+vaishu711@users.noreply.github.com>
2018-02-12 15:46:16 +0530

    QUICK.C

---
+++ QUICK.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a[30],n,i;
+	printf("Enter no of elements:");
+	scanf(i=0;i<n;i++)
+	scanf("%d",&a[i]);
+	mergesort(a,0,n-1);
+	printf("\n sorted array is:");
+	for(i=0;i<n;i++)
+	printf("%d",a[i]);
+	return 0;
+}
