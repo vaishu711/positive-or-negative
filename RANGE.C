239e9c19542e911ac0122bd11e0ba7cd767e5fb9
vaishu711 <35526147+vaishu711@users.noreply.github.com>
2018-03-02 16:04:02 +0530

    RANGE.C
    TO CHECK WHETHER THE GIVEN NUMBER IS IN THE RANGE

---
+++ RANGE.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int n=8,i;
+printf("Enter any number n:");
+scanf("%d",&n);
+for(i=1;i<=10;i++)
+{
+if(i<10)
+{
+printf("YES");
+}
+else
+{
+printf("NO");
+return 0;
+}	
+}
+}
