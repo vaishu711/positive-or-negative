2a32a47e08eee489bc4a094ae95056797d6ab1ad
vaishu711 <35526147+vaishu711@users.noreply.github.com>
2018-03-05 16:41:14 +0530

    SUM.C
    TO CALCULATE SUM OF TWO INTEGER

---
+++ SUM.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b,sum;
+printf("Enter the value of a:");
+scanf("%d",&a);
+printf("Enter the value of b:");
+scanf("%d",&b);
+sum=a+b;
+printf("\n Sum of %d and %d is:%d",a,b,sum);
+return 0;
+}
