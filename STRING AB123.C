10f0e7221687eb07035cf3d951314781dc3c73f6
vaishu711 <35526147+vaishu711@users.noreply.github.com>
2018-03-09 16:37:42 +0530

    STRING AB123
    TO FIND WHETHER THE GIVEN STRING CONTAINS BOTH ALPHABET & NUMBER

---
+++ STRING AB123.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int s;
+printf(“Enter the string:”);
+scanf(“%s”,&s);
+s=hello123;
+if((s>=’A’&&s<=’Z’)||(s>=’a’&&s<=’z’&&s>=’0’&&s<=’9′))
+printf(“\nYes”);
+else
+printf(“\nNo”);
+return 0;
+}
